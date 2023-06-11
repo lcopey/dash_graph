ae98e313a69a01aff1a43b85c88360ab5166580c
laurent <laurent.copey@gmail.com>
2023-06-11 12:08:44 +0200

    pushing onlineé

---
+++ dynamic_graphs.py
@@ -0,0 +1,158 @@
+from typing import Tuple, List, Dict
+from types import MappingProxyType
+
+from dataclasses import dataclass
+import dash
+import dash_bootstrap_components as dbc
+import pandas as pd
+import plotly.express as px
+from dash import Input, Output, callback, dcc, html, State
+
+
+@dataclass(frozen=True)
+class ControlAttribute:
+    name: str
+    label: str
+    kwargs: str
+
+
+class SequenceOfControls(tuple):
+    def __new__(cls, iterable):
+        items = {}
+        for item in iterable:
+            assert isinstance(item, ControlAttribute)
+            items[item.name] = item
+
+        instance = super().__new__(cls, iterable)
+        instance._items = MappingProxyType(items)
+        return instance
+
+    def __getitem__(self, key) -> ControlAttribute:
+        if isinstance(key, (int, slice)):
+            return super().__getitem__(key)
+        elif isinstance(key, str):
+            return self._items[key]
+
+    @property
+    def names(self) -> Tuple[str]:
+        return tuple(item.name for item in self)
+
+    @property
+    def labels(self) -> Tuple[str]:
+        return tuple(item.label for item in self)
+
+    @property
+    def kwargs(self) -> Tuple[str]:
+        return tuple(item.kwargs for item in self)
+
+
+class BaseIds:
+    def __init__(self, aio_id: str):
+        self.aio_id = aio_id
+
+    def generate_id(self, subcomponent: str):
+        return f'{self.aio_id}-{subcomponent}'
+
+
+CONTROLS = SequenceOfControls(
+    [ControlAttribute('xaxis', 'x-axis :', 'x'),
+     ControlAttribute('yaxis', 'y-axis :', 'y'),
+     ControlAttribute('color', 'Legend :', 'color'),
+     ControlAttribute('values', 'Values :', 'values'),
+     ControlAttribute('names', 'Names :', 'names')]
+)
+
+FIGURES = {'bar': px.bar, 'scatter': px.scatter, 'line': px.line, 'pie': px.pie}
+CONTROLS_PER_CHART_TYPE = {'bar': ['xaxis', 'yaxis', 'color'],
+                           'scatter': ['xaxis', 'yaxis', 'color'],
+                           'line': ['xaxis', 'yaxis', 'color'],
+                           'pie': ['values', 'names']}
+
+
+def get_select_component(group_id: str,
+                         select_id: str,
+                         label: str,
+                         options: List[Dict[str, str]] | List[str] | None = None,
+                         value: str | None = None):
+    children = (dbc.InputGroupText(label, style={'width': '40%'}),
+                dbc.Select(id=select_id, options=options, value=value))
+    return dbc.InputGroup(id=group_id, children=children)
+
+
+class DashGraphIds(BaseIds):
+    def select(self, name: str, group_level: bool = False):
+        if group_level:
+            name = f'{name}-group'
+        return self.generate_id(name)
+
+    def figure(self):
+        return self.generate_id('figure')
+
+    def chart_type(self, group_level: bool = False):
+        return self.select('chart_type', group_level)
+
+
+class DashGraphAIO(html.Div):
+
+    def _select(self, name: str, options=None, value=None):
+        return get_select_component(group_id=self.ids.select(name, True),
+                                    select_id=self.ids.select(name, False),
+                                    label=CONTROLS[name].label,
+                                    options=options,
+                                    value=value)
+
+    def __init__(self, aio_id: str, source_store_id: str):
+        self.ids = DashGraphIds(aio_id=aio_id)
+
+        figure = dcc.Graph(id=self.ids.figure())
+        chart_type = get_select_component(group_id=self.ids.chart_type(True),
+                                          select_id=self.ids.chart_type(False),
+                                          label='Chart type :',
+                                          options=list(FIGURES.keys()),
+                                          value='bar')
+
+        chart_controls = [self._select(name) for name in CONTROLS.names]
+
+        layout = dbc.Row(
+            [
+                dbc.Col([chart_type, *chart_controls], width=4),
+                dbc.Col(figure, width=8)]
+        )
+        super().__init__(layout)
+
+        @callback(
+            [Output(self.ids.select(name), 'options') for name in CONTROLS.names],
+            Input(source_store_id, 'data')
+        )
+        def synchronize_selection(data):
+            data = pd.DataFrame(data)
+            selection = [{'value': c, 'label': c} for c in data.columns]
+            return (selection,) * len(CONTROLS)
+
+        @callback(
+            [Output(self.ids.select(name, True), 'style') for name in CONTROLS.names],
+            Input(self.ids.chart_type(), 'value'))
+        def update_control_visibility(chart_type_value):
+            controls = CONTROLS_PER_CHART_TYPE[chart_type_value]
+            displayed = [{'display': 'none' if name not in controls else 'flex'}
+                         for name in CONTROLS.names]
+            return displayed
+
+        @callback(
+            Output(figure, 'figure'),
+            Input(self.ids.chart_type(), 'value'),
+            State(source_store_id, 'data'),
+            [Input(self.ids.select(name), 'value') for name in CONTROLS.names]
+        )
+        def update_graph(chart_type_value, data, *args):
+            if chart_type_value:
+                data = pd.DataFrame(data)
+                kwargs = dict(zip(CONTROLS.kwargs, args))
+                print(kwargs)
+                # select relevant options
+                for name in CONTROLS.names:
+                    if name not in CONTROLS_PER_CHART_TYPE[chart_type_value]:
+                        kwargs.pop(CONTROLS[name].kwargs)
+                func = FIGURES[chart_type_value]
+                return func(data, **kwargs)
+            return dash.no_update
---
+++ main.py
@@ -0,0 +1,17 @@
+import dash_bootstrap_components as dbc
+import pandas as pd
+from dash import Dash, dcc, html
+
+# from pivot_graph import DashGraphAIO
+# from graphs import DashGraphAIO
+from dynamic_graphs import DashGraphAIO
+
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+datas = pd.read_parquet('./iris.parquet')
+
+store = dcc.Store(id='store', data=datas.to_dict('records'))
+dash_graph = DashGraphAIO('main', source_store_id='store')
+app.layout = html.Div([dash_graph, store])
+
+if __name__ == '__main__':
+    app.run(port=8050, debug=True)
